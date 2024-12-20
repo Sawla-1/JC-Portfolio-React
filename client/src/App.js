f09ab7a0f80eebdce48f93caa9832d776ce34fe7
Sha Doh <ncboyboyboy@gmail.com>
2024-12-20 19:28:51 +0700

    upload photos in assets folder, update html, App and index Js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import Home from "./pages/home/Home";
+import SubClasses from "./pages/subClasses/SubClasses";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/subClasses" element={<SubClasses />} />
+    </Routes>
+    </Router>
   );
 }
 
